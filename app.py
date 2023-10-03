d862467d606bcba2f201392ff33becdd7cf2df6e
valenfersartir <nelav2187@gmail.com>
2023-10-03 08:59:55 +0700

    3oct2023

---
+++ app.py
@@ -0,0 +1,18 @@
+from  flask import Flask, render_template
+
+app = Flask(__name__)
+
+students = [
+    {"name": "Sandrine", "score": 100},
+    {"name": "Gergeley", "score": 87},
+    {"name": "Frieda", "score": 92},
+    {"name": "Fritz", "score": 40},
+    {"name": "Sirius", "score": 75},
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html", students=students)
+
+if __name__ == "__main__":
+    app.run(debug=True)
